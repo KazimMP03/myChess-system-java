336bffb9b9138435cc89b80b8468d4a575ef3adc
Caio Martins Pinheiro <caiomp10@gmail.com>
2024-06-24 12:17:48 -0300

    Printing possible moves on board

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,6 +21,10 @@ public class Program {
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+                
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
@@ -34,7 +38,7 @@ public class Program {
 			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			}
+			}   
         }
         
     }
